2147bf1e3b2616007344959c644b5b0a04e24018
joel alonzo <joelangeloalonzo@gmail.com>
2020-03-17 17:10:50 -0400

    First commit

---
+++ src/main/java/com/qte/segridsidecar/SegridsidecarApplication.java
@@ -0,0 +1,13 @@
+package com.qte.segridsidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegridsidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegridsidecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qte/segridsidecar/SegridsidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qte.segridsidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegridsidecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
